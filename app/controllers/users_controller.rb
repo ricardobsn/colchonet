751d88bd2112c42a8b273d1e7c9a6313c2efea30
Ricardo Neves <ricardo.neves@m4u.com.br>
2017-03-06 15:19:00 -0300

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,30 @@
 class UsersController < ApplicationController
+  # chama a view por default no users new (caso não especificado)
 
-# chama a view por default no users new (caso não especificado)
+  def show
+  @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
-  # método create o params q é o importante, caso de erro, ele faz reload da pag de criação
+  # método create, o params q é o importante, caso de erro, ele faz reload da pag de criação
   def create
-    @user = User.new(params[:user])
+    @user = User.new(params[user_params])
     if @user.save
       redirect_to @user,
                   notice: 'Cadastro criado com sucesso!'
     else
       render action: :new
     end
+  end
+
+  private
+
+  def user_params
+    params.
+      require(:user).
+      permit(:email, :full_name, :location, :password, :password_confirmation, :bio)
   end
 end
