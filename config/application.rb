afff672f2eb21b5e384f6ca4cc8a2346b526f947
Ricardo Neves <ricardo.neves@m4u.com.br>
2017-03-10 10:19:27 -0300

    wip linguagem suportada

--- config/application.rb
+++ config/application.rb
@@ -2,22 +2,19 @@ require_relative 'boot'
 
 require 'rails/all'
 
-
-
 Bundler.require(*Rails.groups)
 
 module Colchonet
   class Application < Rails::Application
     # Set Time.zone default to the specified zone
-    #and make Active Record auto-convert to this zone.
+    # and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of
-    #tasks for finding time zone names. Default is UTC.
+    # tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Brasilia'
     # The default locale is :en and all
-    #translations from config/locales/*.rb,yml are auto loaded.
+    # translations from config/locales/*.rb,yml are auto loaded.
     # Rails.application.config.i18n.load_path
     #+= Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :pt
-
   end
 end
